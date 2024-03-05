a4b1fe9a7acf19ce480dbc9bd468574d4ba10edf
Enrique Mainero Salinas <emainero.salinas@gmail.com>
2024-03-04 21:26:04 -0600

    Chapter 4 ComposeDemo
    Chapter 4 ComposeDemo

--- composeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ composeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,13 +4,21 @@ import  android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composedemo.ui.theme.ComposeDemoTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Slider
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +29,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //DemoText(message = "Bienvenido Compose", fontSize = 12f )
+                    DemoScreen()
                 }
             }
         }
@@ -29,17 +38,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DemoScreen() {
+
+    var sliderPosition by remember { mutableFloatStateOf(20f) }
+
+    val handlePositionChange = { position : Float ->
+        sliderPosition = position
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        DemoText(message = "Welcome to Compose 1.5", fontSize = sliderPosition)
+        Spacer(modifier = Modifier.height(150.dp))
+        DemoSlider(
+            sliderPosition = sliderPosition,
+            onPositionChange = handlePositionChange
+        )
+        Text(
+            style = MaterialTheme.typography.headlineMedium,
+            text = sliderPosition.toInt().toString() + "sp"
+        )
+    }
+}
+@Composable
+fun DemoText(message: String, fontSize: Float) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..30f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it)}
+    )
+}
+@Preview(showSystemUi = true)
+@Composable
+fun DemoTextPreview() {
     ComposeDemoTheme {
-        Greeting("Android")
+        DemoScreen()
     }
 }
